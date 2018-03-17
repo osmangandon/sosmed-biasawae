9c19c480decba90644410e70b4c77065c4448f3d
agus muhajir <hajirodeon@gmail.com>
2018-03-17 21:53:40 +0700

    v0.5

--- tmp_web5/php-mention-system/config/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-define('DBHost', 'localhost');
-define('DBName', 'biasawae_social_media');
-define('DBUser', 'biasawae');
-define('DBPassword', 'biasawae');
-global $DB;
-require(dirname(__FILE__)."/PDO.class.php");
-$DB = new Db(DBHost, DBName, DBUser, DBPassword);
