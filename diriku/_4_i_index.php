5ea053084fc60c9457f817cb268ffc48de764c6c
agus muhajir <hajirodeon@gmail.com>
2018-03-20 16:05:20 +0700

    v0.6

--- diriku/_4_i_index.php
+++ diriku/_4_i_index.php
@@ -172,95 +172,31 @@ if ((isset($_GET['aksi']) && $_GET['aksi'] == 'simpan'))
 
 	
 	
-	//echo "status : $statuskd";
 
 
 
-	//tampilkan status....
-	$tablenya = "user_status$kd6_session";
-	$qku = mysql_query("SELECT * FROM $tablenya ".
-							"ORDER BY postdate DESC");
-	$rku = mysql_fetch_assoc($qku);
-	$tku = mysql_num_rows($qku);
+	/*
+	echo '<table border="1">
+	<tr valign="top">
+	<td>
 	
-	$ku_kd = nosql($rku['kd']);
-	$ku_userkd = nosql($rku['kd_user']);
-	$ku_postdate = $rku['postdate'];
-	$ku_status = balikin($rku['status']);
-	$ku_urlnya = balikin($rku['urlnya']);
-	$ku_urlnya_judul = balikin($rku['urlnya_judul']);
-	$ku_urlnya_image = balikin($rku['urlnya_image']);
-	$ku_urlnya_deskripsi = balikin($rku['urlnya_deskripsi']);
-
-	?>
+	<table border="0">
+	<tr valign="top">
+	<td width="100">
+	<img src="'.$image.'" alt="'.$title.'" width="100" height="100">
+	</td>
 	
-
-	<script language='javascript'>
-	//membuat document jquery
-	$(document).ready(function(){
-
-		$("#ikutkomen<?php echo $ku_kd;?>").load("<?php echo $filenyax;?>?aksi=ikutkomen&kdnya=<?php echo $ku_kd;?>");			
-				
-	});
+	<td>
+	<h3>'.$title.'</h3>
+	<p><i>'.$description.'</i></p>
+	</td>
+	</tr>
+	</table>
 	
-	</script>
-
-	
-	<?php
-
-	echo '<form name="formx'.$ku_kd.'" id="formx'.$ku_kd.'">
-	<div class="panel panel-default">
-    <div class="panel-heading panel-heading-custom">
-	<table border="0" cellspading="3" cellspacing="3">
-	<tr>
-	<td>		
-	'.$yukx_nama.'
-	<br>
-	'.$ku_postdate.'
-	<br>
-	<h3>'.$ku_status.'</h3>';
-	
-	
-	
-	
-		//jika ada
-	if (!empty($ku_urlnya_judul))
-		{
-		echo '<table border="1">
-		<tr valign="top">
-		<td>
-		
-		<table border="0">
-		<tr valign="top">
-		<td width="100">
-		<img src="'.$ku_urlnya_image.'" alt="'.$ku_urlnya_judul.'" width="100" height="100">
-		</td>
-		
-		<td>
-		<h3>'.$ku_urlnya_judul.'</h3>
-		<p><i>'.$ku_urlnya_deskripsi.'</i></p>
-		</td>
-		</tr>
-		</table>
-		
-		</td>
-		</tr>
-		</table>';
-		}
-		 
-	
-	echo '<div id="ikutkomen'.$ku_kd.'"></div>
 	</td>
 	</tr>
 	</table>';
-
-
-
-
-
-
-
-
+	*/
 		
 	$doc->clear(); 
 	unset($doc);
@@ -355,182 +291,12 @@ if ((isset($_GET['aksi']) && $_GET['aksi'] == 'simpan2'))
 
 
 //jika daftar
-if ((isset($_GET['aksi']) && $_GET['aksi'] == 'daftar2'))
+if ((isset($_GET['aksi']) && $_GET['aksi'] == 'daftar'))
 	{
 	//daftar
 	$tablenya = "user_status$kd6_session";
 	$qku = mysql_query("SELECT * FROM $tablenya ".
-							"ORDER BY postdate DESC LIMIT 0,5");
-	$rku = mysql_fetch_assoc($qku);
-	$tku = mysql_num_rows($qku);
-	
-	
-	//jika gak null
-	if (!empty($tku))
-		{
-		do
-			{
-			$ku_kd = nosql($rku['kd']);
-			$ku_userkd = nosql($rku['kd_user']);
-			$ku_postdate = $rku['postdate'];
-			$ku_status = balikin($rku['status']);
-			$ku_urlnya = balikin($rku['urlnya']);
-			$ku_urlnya_judul = balikin($rku['urlnya_judul']);
-			$ku_urlnya_image = balikin($rku['urlnya_image']);
-			$ku_urlnya_deskripsi = balikin($rku['urlnya_deskripsi']);
-			
-			
-			?>
-			
-			
-		
-			<script language='javascript'>
-			//membuat document jquery
-			$(document).ready(function(){
-
-
-			$("#idaftarkom<?php echo $ku_kd;?>").load("<?php echo $filenyax;?>?aksi=daftarkom&kdnya=<?php echo $ku_kd;?>");			
-
-
-
-			$("#linknya").on('click', function(){
-
-						
-				$.ajax({
-					url: "<?php echo $filenyax;?>?aksi=daftarlama",
-					type:$(this).attr("method"),
-					data:$(this).serialize(),
-					success:function(data){			
-					    
-					    $("#idaftar").append(data);
-						
-						}
-					});
-				return false;
-			})
-	
-
-
-					
-			});
-			
-			</script>
-	
-			
-			<?php		
-			//usernya
-			//detail user
-			$qyukx = mysql_query("SELECT * FROM m_user ".
-									"WHERE kd = '$ku_userkd'");
-			$ryukx = mysql_fetch_assoc($qyukx);
-			$yukx_nama = balikin($ryukx['nama']);
-			
-			echo '<form name="formx'.$ku_kd.'" id="formx'.$ku_kd.'">
-			<div class="panel panel-default">
-		    <div class="panel-heading panel-heading-custom">
-			<table border="0" cellspading="3" cellspacing="3">
-			<tr>
-			<td>		
-			'.$yukx_nama.'
-			<br>
-			'.$ku_postdate.'
-			<br>
-			<h3>'.$ku_status.'</h3>';
-			
-			
-
-			
-			//jika ada
-			if (!empty($ku_urlnya_judul))
-				{
-				echo '<table border="1">
-				<tr valign="top">
-				<td>
-				
-				<table border="0">
-				<tr valign="top">
-				<td width="100">
-				<img src="'.$ku_urlnya_image.'" alt="'.$ku_urlnya_judul.'" width="100" height="100">
-				</td>
-				
-				<td>
-				<h3>'.$ku_urlnya_judul.'</h3>
-				<p><i>'.$ku_urlnya_deskripsi.'</i></p>
-				</td>
-				</tr>
-				</table>
-				
-				</td>
-				</tr>
-				</table>';
-				}
-			
-			 
-			echo '[<a href="#">IKUTI</a>].
-			<hr>
-	
-	
-			<div id="idaftarkom'.$ku_kd.'"></div>';
-	
-			
-			
-			echo '</div>
-			</div>
-			</form>
-
-			
-			</td>
-			</tr>
-			</table>';
-			}
-		while ($rku = mysql_fetch_assoc($qku));
-		
-		
-		
-			
-			
-		echo '<br>
-		<br>
-		
-		
-		[<a href="#" id="linknya">TAMPILKAN STATUS LAMA</a>]...';
-		}	
-	
-	exit();
-	}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//jika daftar lama
-if ((isset($_GET['aksi']) && $_GET['aksi'] == 'daftarlama'))
-	{
-	//nilai page...
-	$pagenya = nosql($_REQUEST['pagenya']);
-	
-	
-	//awal
-	$nilawal = 5;
-	//$nilakhir = 10 * $pagenya;
-	$nilakhir = 9;
-	
-	
-	//daftar
-	$tablenya = "user_status$kd6_session";
-	$qku = mysql_query("SELECT * FROM $tablenya ".
-							"ORDER BY postdate DESC LIMIT $nilawal,$nilakhir");
+							"ORDER BY postdate DESC");
 	$rku = mysql_fetch_assoc($qku);
 	$tku = mysql_num_rows($qku);
 	
@@ -614,16 +380,31 @@ if ((isset($_GET['aksi']) && $_GET['aksi'] == 'daftarlama'))
 				</tr>
 				</table>';
 				}
-			
 			 
-			echo '[<a href="#">IKUTI</a>].
-			<hr>
-	
-
-			<div id="idaftarkom'.$ku_kd.'"></div>';
+			 
+			echo '<br> 
+			[IKUTI]. [jumlah pengikut]
+			</td>
+			</tr>
+			</table>';
 	
 			
-			
+			//jika gak null
+			if (!empty($tku))
+				{
+				echo '<table border="0" cellspading="3" cellspacing="3">
+				<tr>
+				<td width="50"></td>
+				<td>
+	
+				<div id="idaftarkom'.$ku_kd.'"></div>
+				
+				
+				</td>
+				</tr>
+				</table>';
+				}
+				
 			echo '</div>
 			</div>
 			</form>
@@ -634,26 +415,10 @@ if ((isset($_GET['aksi']) && $_GET['aksi'] == 'daftarlama'))
 			</table>';
 			}
 		while ($rku = mysql_fetch_assoc($qku));
-		
-		
-		
-			
-			
-		echo '<br>
-		<br>';
-		
-		
-		exit();
 		}	
 	
 	exit();
 	}
-
-
-
-
-
-
 
 
 
@@ -672,7 +437,27 @@ if ((isset($_GET['aksi']) && $_GET['aksi'] == 'daftarkom'))
 
 	?>
 
+		
 
+
+
+	<!-- Bootstrap core JavaScript -->
+	<script src="../template/vendor/jquery/jquery.min.js"></script>
+	<script src="../template/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+	
+	
+	<!-- Bootstrap core CSS -->
+	<link href="../template/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+
+    <link rel="stylesheet" href="../inc/js/jquery-ui-1.10.3/themes/black-tie/jquery-ui.css">
+    <link rel="stylesheet" href="../inc/js/jquery.mentions.css">
+    <script src="../inc/js/jquery-ui-1.10.3/ui/jquery-ui.js"></script>
+    <script src="../inc/js/jquery.mentions.js"></script>
+
+
+	
 
 	<script language='javascript'>
 	//membuat document jquery
@@ -688,6 +473,8 @@ if ((isset($_GET['aksi']) && $_GET['aksi'] == 'daftarkom'))
 				type:$(this).attr("method"),
 				data:$(this).serialize(),
 				success:function(data){					
+					$("#itulisresult<?php echo $ekdnya;?>").html(data);
+					
 					$("#idaftarkom<?php echo $ekdnya;?>").load("<?php echo $filenyax;?>?aksi=daftarkom&kdnya=<?php echo $ekdnya;?>");
 					
 					setTimeout('$("#loading<?php echo $ekdnya;?>").hide()',1000);
@@ -712,11 +499,49 @@ if ((isset($_GET['aksi']) && $_GET['aksi'] == 'daftarkom'))
 
 
 	<?php	
-	echo '<table border="0" cellspading="3" cellspacing="3">
-	<tr>
-	<td width="50"></td>
-	<td>';
 	
+	/*
+	//daftar komentar...
+	$tablenya = "user_status_msg$kd6_session";
+	$qku2 = mysql_query("SELECT * FROM $tablenya ".
+							"WHERE kd_user_status = '$ekdnya' ".
+							"ORDER BY postdate ASC");
+	$rku2 = mysql_fetch_assoc($qku2);
+	
+	do
+		{
+		$ku2_kd = nosql($rku2['kd']);
+		$ku2_dari = nosql($rku2['dari']);
+		$ku2_komentar = balikin($rku2['msg']);
+		$ku2_postdate = $rku2['postdate'];
+		
+		//detail user
+		$qyuk = mysql_query("SELECT * FROM m_user ".
+								"WHERE kd = '$ku2_dari'");
+		$ryuk = mysql_fetch_assoc($qyuk);
+		$yuk_nama = balikin($ryuk['nama']);
+		
+		echo '<div class="panel panel-default">
+	    <div class="panel-body panel-body-custom">
+		
+		x
+		<p>
+		[<b>'.$yuk_nama.'</b>]
+		<br>
+		'.$ku2_komentar.'
+		 <br>
+		['.$ku2_postdate.']
+		</p>
+
+		</div>
+		
+		</div>';	
+		}
+	while ($rku2 = mysql_fetch_assoc($qku2));
+	*/
+	
+	
+					
 	//daftar komentar...
 	$tablenya = "user_status_msg$kd6_session";
 	$qku2 = mysql_query("SELECT * FROM $tablenya ".
@@ -798,13 +623,7 @@ if ((isset($_GET['aksi']) && $_GET['aksi'] == 'daftarkom'))
 	<input name="btnKOM'.$ekdnya.'" id="btnKOM'.$ekdnya.'" type="submit" class="btn btn-info" value="KIRIM >>">
 	</p>
 		 
-	<hr>
-	
-	
-	</td>
-	
-	</tr>
-	</table>';
+	<hr>';
 	
 
 				
@@ -815,40 +634,6 @@ if ((isset($_GET['aksi']) && $_GET['aksi'] == 'daftarkom'))
 
 
 
-
-
-//jika daftar status
-if ((isset($_GET['aksi']) && $_GET['aksi'] == 'daftar'))
-	{
-	?>
-	
-
-	<script language='javascript'>
-	//membuat document jquery
-	$(document).ready(function(){
-	
-		$.ajax({
-			url: "<?php echo $filenyax;?>?aksi=daftar2",
-			type:$(this).attr("method"),
-			data:$(this).serialize(),
-			success:function(data){			
-			    
-			    $("#idaftar").prepend(data);
-
-				}
-			});
-		return false;
-
-	
-	});
-	
-	</script>
-	
-	
-
-	<?php
-	exit();
-	}
 
 
 
@@ -871,11 +656,13 @@ if ((isset($_GET['aksi']) && $_GET['aksi'] == 'form'))
 					url: "<?php echo $filenyax;?>?aksi=simpan",
 					type:$(this).attr("method"),
 					data:$(this).serialize(),
-					success:function(data){			
-					    
-					    $("#idaftar").prepend(data);
-
+					success:function(data){					
+						$("#itulisresult").html(data);
+						
+						$("#idaftar").load("<?php echo $filenyax;?>?aksi=daftar");
 						$("#itulis").load("<?php echo $filenyax;?>?aksi=form");
+		
+				
 
 
 				
@@ -959,38 +746,4 @@ if ((isset($_GET['aksi']) && $_GET['aksi'] == 'form'))
 	exit();
 	}
 
-
-
-
-
-//jika ikut komentar
-if ((isset($_GET['aksi']) && $_GET['aksi'] == 'ikutkomen'))
-	{
-	$ekdnya = cegah($_REQUEST['kdnya']);
-
-	?>
-
-	<script language='javascript'>
-	//membuat document jquery
-	$(document).ready(function(){
-
-
-		$("#idaftarkom<?php echo $ekdnya;?>").load("<?php echo $filenyax;?>?aksi=daftarkom&kdnya=<?php echo $ekdnya;?>");			
-	
-				
-	});
-	
-	</script>
-
-
-	<?php
-
-	echo '[<a href="#">IKUTI</a>].
-	<hr>
-	
-	
-	<div id="idaftarkom'.$ekdnya.'"></div>';
-	
-	exit();
-	}
 ?>
--- diriku/_6_i_index.php
+++ diriku/_6_i_index.php
@@ -360,177 +360,7 @@ if ((isset($_GET['aksi']) && $_GET['aksi'] == 'daftar2'))
 	//daftar
 	$tablenya = "user_status$kd6_session";
 	$qku = mysql_query("SELECT * FROM $tablenya ".
-							"ORDER BY postdate DESC LIMIT 0,5");
-	$rku = mysql_fetch_assoc($qku);
-	$tku = mysql_num_rows($qku);
-	
-	
-	//jika gak null
-	if (!empty($tku))
-		{
-		do
-			{
-			$ku_kd = nosql($rku['kd']);
-			$ku_userkd = nosql($rku['kd_user']);
-			$ku_postdate = $rku['postdate'];
-			$ku_status = balikin($rku['status']);
-			$ku_urlnya = balikin($rku['urlnya']);
-			$ku_urlnya_judul = balikin($rku['urlnya_judul']);
-			$ku_urlnya_image = balikin($rku['urlnya_image']);
-			$ku_urlnya_deskripsi = balikin($rku['urlnya_deskripsi']);
-			
-			
-			?>
-			
-			
-		
-			<script language='javascript'>
-			//membuat document jquery
-			$(document).ready(function(){
-
-
-			$("#idaftarkom<?php echo $ku_kd;?>").load("<?php echo $filenyax;?>?aksi=daftarkom&kdnya=<?php echo $ku_kd;?>");			
-
-
-
-			$("#linknya").on('click', function(){
-
-						
-				$.ajax({
-					url: "<?php echo $filenyax;?>?aksi=daftarlama",
-					type:$(this).attr("method"),
-					data:$(this).serialize(),
-					success:function(data){			
-					    
-					    $("#idaftar").append(data);
-						
-						}
-					});
-				return false;
-			})
-	
-
-
-					
-			});
-			
-			</script>
-	
-			
-			<?php		
-			//usernya
-			//detail user
-			$qyukx = mysql_query("SELECT * FROM m_user ".
-									"WHERE kd = '$ku_userkd'");
-			$ryukx = mysql_fetch_assoc($qyukx);
-			$yukx_nama = balikin($ryukx['nama']);
-			
-			echo '<form name="formx'.$ku_kd.'" id="formx'.$ku_kd.'">
-			<div class="panel panel-default">
-		    <div class="panel-heading panel-heading-custom">
-			<table border="0" cellspading="3" cellspacing="3">
-			<tr>
-			<td>		
-			'.$yukx_nama.'
-			<br>
-			'.$ku_postdate.'
-			<br>
-			<h3>'.$ku_status.'</h3>';
-			
-			
-
-			
-			//jika ada
-			if (!empty($ku_urlnya_judul))
-				{
-				echo '<table border="1">
-				<tr valign="top">
-				<td>
-				
-				<table border="0">
-				<tr valign="top">
-				<td width="100">
-				<img src="'.$ku_urlnya_image.'" alt="'.$ku_urlnya_judul.'" width="100" height="100">
-				</td>
-				
-				<td>
-				<h3>'.$ku_urlnya_judul.'</h3>
-				<p><i>'.$ku_urlnya_deskripsi.'</i></p>
-				</td>
-				</tr>
-				</table>
-				
-				</td>
-				</tr>
-				</table>';
-				}
-			
-			 
-			echo '[<a href="#">IKUTI</a>].
-			<hr>
-	
-	
-			<div id="idaftarkom'.$ku_kd.'"></div>';
-	
-			
-			
-			echo '</div>
-			</div>
-			</form>
-
-			
-			</td>
-			</tr>
-			</table>';
-			}
-		while ($rku = mysql_fetch_assoc($qku));
-		
-		
-		
-			
-			
-		echo '<br>
-		<br>
-		
-		
-		[<a href="#" id="linknya">TAMPILKAN STATUS LAMA</a>]...';
-		}	
-	
-	exit();
-	}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//jika daftar lama
-if ((isset($_GET['aksi']) && $_GET['aksi'] == 'daftarlama'))
-	{
-	//nilai page...
-	$pagenya = nosql($_REQUEST['pagenya']);
-	
-	
-	//awal
-	$nilawal = 5;
-	//$nilakhir = 10 * $pagenya;
-	$nilakhir = 9;
-	
-	
-	//daftar
-	$tablenya = "user_status$kd6_session";
-	$qku = mysql_query("SELECT * FROM $tablenya ".
-							"ORDER BY postdate DESC LIMIT $nilawal,$nilakhir");
+							"ORDER BY postdate DESC");
 	$rku = mysql_fetch_assoc($qku);
 	$tku = mysql_num_rows($qku);
 	
@@ -614,16 +444,31 @@ if ((isset($_GET['aksi']) && $_GET['aksi'] == 'daftarlama'))
 				</tr>
 				</table>';
 				}
-			
 			 
-			echo '[<a href="#">IKUTI</a>].
-			<hr>
-	
-
-			<div id="idaftarkom'.$ku_kd.'"></div>';
+			 
+			echo '<br> 
+			[IKUTI]. [jumlah pengikut]
+			</td>
+			</tr>
+			</table>';
 	
 			
-			
+			//jika gak null
+			if (!empty($tku))
+				{
+				echo '<table border="0" cellspading="3" cellspacing="3">
+				<tr>
+				<td width="50"></td>
+				<td>
+	
+				<div id="idaftarkom'.$ku_kd.'"></div>
+				
+				
+				</td>
+				</tr>
+				</table>';
+				}
+				
 			echo '</div>
 			</div>
 			</form>
@@ -634,26 +479,10 @@ if ((isset($_GET['aksi']) && $_GET['aksi'] == 'daftarlama'))
 			</table>';
 			}
 		while ($rku = mysql_fetch_assoc($qku));
-		
-		
-		
-			
-			
-		echo '<br>
-		<br>';
-		
-		
-		exit();
 		}	
 	
 	exit();
 	}
-
-
-
-
-
-
 
 
 
@@ -672,7 +501,27 @@ if ((isset($_GET['aksi']) && $_GET['aksi'] == 'daftarkom'))
 
 	?>
 
+		
 
+
+
+	<!-- Bootstrap core JavaScript -->
+	<script src="../template/vendor/jquery/jquery.min.js"></script>
+	<script src="../template/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+	
+	
+	<!-- Bootstrap core CSS -->
+	<link href="../template/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+
+    <link rel="stylesheet" href="../inc/js/jquery-ui-1.10.3/themes/black-tie/jquery-ui.css">
+    <link rel="stylesheet" href="../inc/js/jquery.mentions.css">
+    <script src="../inc/js/jquery-ui-1.10.3/ui/jquery-ui.js"></script>
+    <script src="../inc/js/jquery.mentions.js"></script>
+
+
+	
 
 	<script language='javascript'>
 	//membuat document jquery
@@ -688,6 +537,8 @@ if ((isset($_GET['aksi']) && $_GET['aksi'] == 'daftarkom'))
 				type:$(this).attr("method"),
 				data:$(this).serialize(),
 				success:function(data){					
+					$("#itulisresult<?php echo $ekdnya;?>").html(data);
+					
 					$("#idaftarkom<?php echo $ekdnya;?>").load("<?php echo $filenyax;?>?aksi=daftarkom&kdnya=<?php echo $ekdnya;?>");
 					
 					setTimeout('$("#loading<?php echo $ekdnya;?>").hide()',1000);
@@ -815,40 +666,6 @@ if ((isset($_GET['aksi']) && $_GET['aksi'] == 'daftarkom'))
 
 
 
-
-
-//jika daftar status
-if ((isset($_GET['aksi']) && $_GET['aksi'] == 'daftar'))
-	{
-	?>
-	
-
-	<script language='javascript'>
-	//membuat document jquery
-	$(document).ready(function(){
-	
-		$.ajax({
-			url: "<?php echo $filenyax;?>?aksi=daftar2",
-			type:$(this).attr("method"),
-			data:$(this).serialize(),
-			success:function(data){			
-			    
-			    $("#idaftar").prepend(data);
-
-				}
-			});
-		return false;
-
-	
-	});
-	
-	</script>
-	
-	
-
-	<?php
-	exit();
-	}
 
 
 
@@ -985,7 +802,7 @@ if ((isset($_GET['aksi']) && $_GET['aksi'] == 'ikutkomen'))
 
 	<?php
 
-	echo '[<a href="#">IKUTI</a>].
+	echo '[<a href="#">IKUTI</a>]. [<a href="#">KOMENTAR</a>].
 	<hr>
 	
 	
